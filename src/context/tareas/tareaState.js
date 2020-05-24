b46a3c413685f555be0a4f3795c821825ecbfe3f
gvillegasc <villegasgerardo.j@gmail.com>
2020-05-24 12:32:36 -0500

    Creando estructura de tarea

---
+++ src/context/tareas/tareaState.js
@@ -0,0 +1,16 @@
+import React, { useReducer } from 'react';
+import TareaContext from './tareaContext';
+import TareaReducer from './tareaReducer';
+
+const TareaState = (props) => {
+	const initalState = {
+		tareas: [],
+	};
+
+	// Crear dispatch y state
+	const [state, dispatch] = useReducer(TareaReducer, initalState);
+
+	return <TareaContext.Provider>{props.children}</TareaContext.Provider>;
+};
+
+export default TareaState;
