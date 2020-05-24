f25a49592289dff18d71edddeb2a465990849297
gvillegasc <villegasgerardo.j@gmail.com>
2020-05-24 17:55:24 -0500

    Funcion para crear tarea

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,10 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
 
 const FormTarea = () => {
 	// Extraer si un proyectoe sta activo
 	const proyectosContext = useContext(proyectoContext);
 	const { proyecto } = proyectosContext;
+
+	// Obtener la funcon del context de tarea
+	const tareasContext = useContext(tareaContext);
+	const { agregarTarea } = tareasContext;
+
+	// State del formulario
+	const [tarea, guardarTarea] = useState({
+		nombre: '',
+	});
+
+	// extraer el nombre de la tarea
+	const { nombre } = tarea;
 
 	// Si no hay proyecto seleccionado
 	if (!proyecto) return null;
@@ -12,15 +25,40 @@ const FormTarea = () => {
 	// Arrary destructurinf para extraer el proyecto actual
 	const [proyectoActual] = proyecto;
 
+	// Leer los valores del formulario
+	const handleChange = (e) => {
+		guardarTarea({
+			...tarea,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		// validar
+
+		// pasar la validación
+
+		// agregar la nueva tarea al state de tareas
+		tarea.proyectoId = proyectoActual.id;
+		tarea.estado = false;
+		agregarTarea(tarea);
+
+		// reiniciar el form
+	};
+
 	return (
 		<div className="formulario">
-			<form>
+			<form onSubmit={onSubmit}>
 				<div className="contenedor-input">
 					<input
 						type="text"
 						className="input-text"
 						placeholder="Nombre Tarea..."
 						name="nombre"
+						value={nombre}
+						onChange={handleChange}
 					></input>
 				</div>
 
