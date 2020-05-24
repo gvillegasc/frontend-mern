37ddf8935ba9cdcb6efbea01d093b8b0df1436d5
gvillegasc <villegasgerardo.j@gmail.com>
2020-05-23 20:28:59 -0500

    Cambiando nombre cuando se selecciona una tarea

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,35 +1,47 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import Tarea from './Tarea';
 
+import proyectoContext from '../../context/proyectos/proyectoContext';
+
 const ListadoTareas = () => {
+	// Extraer proyectos de state inicial
+	const proyectosContext = useContext(proyectoContext);
+	const { proyecto } = proyectosContext;
+
+	// Si no hay proyecto seleccionado
+	if (!proyecto) return <h2>Selecciona un proyecto</h2>;
+
+	// Arrary destructurinf para extraer el proyecto actual
+	const [proyectoActual] = proyecto;
+
 	const tareasProyecto = [
 		{
 			nombre: 'Elegir Plataforma',
-			estado: true
+			estado: true,
 		},
 		{
 			nombre: 'Elegir Colores',
-			estado: false
+			estado: false,
 		},
 		{
 			nombre: 'Elegir Plataforma de pago',
-			estado: false
+			estado: false,
 		},
 		{
 			nombre: 'Elegir Hosting',
-			estado: true
-		}
+			estado: true,
+		},
 	];
 	return (
 		<Fragment>
-			<h2>Proyecto: Tienda Virtual</h2>
+			<h2>Proyecto: {proyectoActual.nombre}</h2>
 			<ul className="listado-tareas">
 				{tareasProyecto.length === 0 ? (
 					<li className="tarea">
 						<p>No hya tareas</p>
 					</li>
 				) : (
-					tareasProyecto.map(tarea => <Tarea tarea={tarea}></Tarea>)
+					tareasProyecto.map((tarea) => <Tarea tarea={tarea}></Tarea>)
 				)}
 			</ul>
 			<button type="button" className="btn btn-eliminar">
